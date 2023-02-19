8ac25b27df48e33add23abccabbc0428bbc6b519
Nikolay Zolotuhin <nik.ward@yandex.ru>
2023-02-19 23:21:48 +0300

    Add finction "get metatype folder"

--- api_yandex.py
+++ api_yandex.py
@@ -1,5 +1,7 @@
 import configparser
 import os.path
+import json
+
 import requests
 
 URL = 'https://cloud-api.yandex.net/v1/disk/resources'
@@ -21,3 +23,14 @@ def create_folder(token, name_folder):
                }
     create_folder = requests.put(URL, params=params, headers=headers)
     return create_folder.status_code
+
+
+def get_metatype_folder(token, name_folder):
+    params = {'path': name_folder}
+    headers = {'Content-Type': 'application/json',
+               'Authorization': token
+               }
+    result = requests.get(URL, headers=headers, params=params)
+    if result.status_code == 200:
+        res_dict = json.loads(result.text)
+        return res_dict.get('type')
