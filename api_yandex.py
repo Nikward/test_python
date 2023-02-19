953ae021b55a50b518dd3f94e105918718ddda87
Nikolay Zolotuhin <nik.ward@yandex.ru>
2023-02-19 22:36:37 +0300

    add module os for path setting.ini

--- api_yandex.py
+++ api_yandex.py
@@ -1,10 +1,22 @@
 import configparser
-
+import os.path
 import requests
+
+URL = 'https://cloud-api.yandex.net/v1/disk/resources'
+
 
 def get_token_ya():
     config = configparser.ConfigParser()
-    config.read("setting.ini")
-    return config['yandex_api']['ya_token']
+    path = os.path.abspath("setting.ini")
+    config.read(path)
+    token = config['yandex_api']['ya_token']
+    return token
 
 
+def create_folder(token, name_folder):
+    params = {'path': name_folder}
+    headers = {'Content-Type': 'application/json',
+               'Authorization': token
+               }
+    create_folder = requests.put(URL, params=params, headers=headers)
+    return create_folder.status_code
